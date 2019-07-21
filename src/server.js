7d83cd1a2e2f94be6d2569c8f81959be195dfa44
Sam Harris <sam@jam-bc.com>
2019-07-21 15:18:39 +0300

    add handler function to server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
-const server = http.createServer();
-
 const port = 4000;
+
+var message = 'hello world';
+
+function handler(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+}
+
+const server = http.createServer(handler);
 
 server.listen(port, function() {
     console.log('the server is running on port ', port);
