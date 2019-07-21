1825674c44a08544fd48a489675a05c799d097a6
Sam Harris <sam@jam-bc.com>
2019-07-21 16:16:58 +0300

    sending image and css response to relevant requests

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
+const path = require("path");
 
 const http = require('http');
 
 const port = 4000;
 
-var message = 'hello world';
-
 
 function handler(request, response) {
     var url = request.url;
 
+    console.log(url);
+
 
     if (url === "/") {
-        
         response.writeHead(200, {"Content-Type": "text/html"});
         fs.readFile(__dirname + '/..' + '/public/index.html', function(error, file) {
             if (error) {
@@ -23,15 +23,41 @@ function handler(request, response) {
                 response.end(file);
             }
         });
-        // response.write(message);
-        // response.end();
-    }
 
-    if (url === "/girl") {
+    } else if (url === "/girl") {
         var message = 'Hello girl';
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write(message);
         response.end();
+    } else if (url === '/node') {
+        var message = 'Hello node';
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(message);
+        response.end();
+    } else if (url === "/main.css") {
+        const filepath = path.join(__dirname, '..', '/public', url);
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile(filepath, function(error, file) {
+            if (error) {
+                console.log(error);
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "text/css"});
+                response.end(file);
+            }
+        });
+    } else if (url === '/img/image.jpg') {
+        const filepath = path.join(__dirname, '..', '/public/', url);
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile(filepath, function(error, file) {
+            if (error) {
+                console.log(error);
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "image/x-icon"});
+                response.end(file);
+            }
+        });
     }
 }
 
