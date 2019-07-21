3e48ae2f9396861cf0d0e2ac56dc68f06d7aa428
Sam Harris <sam@jam-bc.com>
2019-07-21 15:45:32 +0300

    use fs to send html response

--- src/server.js
+++ src/server.js
@@ -1,13 +1,38 @@
+const fs = require("fs");
+
 const http = require('http');
 
 const port = 4000;
 
 var message = 'hello world';
 
+
 function handler(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
+    var url = request.url;
+
+
+    if (url === "/") {
+        
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile(__dirname + '/..' + '/public/index.html', function(error, file) {
+            if (error) {
+                console.log(error);
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.end(file);
+            }
+        });
+        // response.write(message);
+        // response.end();
+    }
+
+    if (url === "/girl") {
+        var message = 'Hello girl';
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(message);
+        response.end();
+    }
 }
 
 const server = http.createServer(handler);
